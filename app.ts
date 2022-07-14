ab88547dac7ba7aee092214454025bfc8f4ff143
IsaPhipho <isaphiladelpho@gmail.com>
2022-07-14 14:18:21 +0100

    Callback: exemplo8

--- app.ts
+++ app.ts
@@ -1,11 +1,19 @@
-// void: ausência de retorno da função (console.log)
-function somarValoresNumericos2(num1: number, num2: number): number {
-  //return num1 + num2.toString();  *Aqui o ts vai sinalizar que a tipagem esta errada!
-  return num1 + num2
-}
-
-console.log(somarValoresNumericos2(1, 3));
-
 function mostraSomaValoresNumericos2(num1: number, num2: number): void {
   console.log(num1 + num2);
 }
+
+function somarValoresNumericos2(num1: number, num2: number, callback: (numero: number) => number): number {
+  let resultado = num1 + num2;
+  return callback(resultado);
+}
+
+function aoQuadrado(num: number): number {
+  return num * num;
+}
+
+function dividirPorEleMesmo(num: number): number {
+  return num / num;
+}
+
+console.log(somarValoresNumericos2(1, 3, aoQuadrado));
+console.log(somarValoresNumericos2(1, 3, dividirPorEleMesmo));
