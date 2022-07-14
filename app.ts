f1f4729f22e7fc3247d4ba17cdcfd6626d778c53
IsaPhipho <isaphiladelpho@gmail.com>
2022-07-14 08:16:34 +0100

    Tipagem: exemplo2

--- app.ts
+++ app.ts
@@ -1,3 +1,22 @@
 let button = document.getElementById('button');
-let input1 = document.getElementById('input1');
-let input2 = document.getElementById('input2');
+let input1 = document.getElementById('input1') as HTMLInputElement;
+let input2 = document.getElementById('input2') as HTMLInputElement;
+
+function adicionarNumeros(num1: number, num2: number, devPrintar: boolean, frase: string) {
+  let resultado = num1 + num2
+  if (devPrintar) {
+    console.log(frase + resultado)
+  }
+  return num1 + num2
+}
+
+let devPrintar = true;
+let frase = 'O valor é: '
+
+if (button) {
+  button.addEventListener('click', () => {
+    if (input1 && input2) {
+      console.log(adicionarNumeros(Number(input1.value), Number(input2.value), devPrintar, frase));
+    }
+  })
+}
